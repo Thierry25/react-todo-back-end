ba75c3388409e04b35af49583a6cff338d003c08
Thierry <marcelinthierry@gmail.com>
2023-01-19 08:38:37 -0500

    Update security configuration file

---
+++ src/main/java/com/thierry/marcelin/restfulservices/controllers/BasicAuth.java
@@ -0,0 +1,18 @@
+package com.thierry.marcelin.restfulservices.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/authenticate")
+public class BasicAuth {
+    
+    @GetMapping
+    public String basicAuthCheck() {
+        return "Success";
+    }
+    
+
+}
--- src/main/java/com/thierry/marcelin/restfulservices/controllers/HelloWorldController.java
+++ src/main/java/com/thierry/marcelin/restfulservices/controllers/HelloWorldController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+
     @GetMapping
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello world");
